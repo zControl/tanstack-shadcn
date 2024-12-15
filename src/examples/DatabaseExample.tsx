e6a3ab9c19904360f4b823183ab339b15e17c555
ZS iMac <zsolis@deltacontrols.com>
2024-12-15 14:30:28 -0800

    Added axios, created apiClient and basic example demo of data from database into table.

--- src/examples/DatabaseExample.tsx
+++ src/examples/DatabaseExample.tsx
@@ -1,25 +1,43 @@
 import { PageSections } from "@/components/common/PageSections";
 import { Tile } from "@/components/composites/Tile";
+import { DataTableCore } from "@/components/data/DataTableCore";
+import { DataTableToolbar } from "@/components/data/DataTableToolbar";
 import { Button } from "@/components/ui/button";
+import { CodeBlock } from "@/components/ui/code-block";
 import { Separator } from "@/components/ui/separator";
 import { Spinner } from "@/components/ui/spinner";
 import { BlockQuote, Header4, Paragraph } from "@/components/ui/typography";
+import { createApiClient } from "@/utils/apiClient";
 import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { CloudDownloadIcon } from "lucide-react";
 
 function CheckApiStatus() {
+  const imp = `import { createApiClient } from "@/utils/apiClient";
+import { useQuery, useQueryClient } from "@tanstack/react-query";`;
+  const code = `const queryClient = useQueryClient();
+const apiClient = createApiClient("http://localhost:3000/v1");
+
+const { isPending, isFetching, isError, data, error } = useQuery({
+    queryKey: ["ApiRoot"],
+    queryFn: async () => apiClient.get(""),
+  });
+
+const handleRefresh = () => {
+  queryClient.invalidateQueries({ queryKey: ["ApiRoot"] });
+};
+`;
+  const usage = `{!isPending && !isError && (
+  <BlockQuote className="max-h-80">
+    <pre>{JSON.stringify(data, null, 2)}</pre>
+  </BlockQuote>
+)}`;
+
   const queryClient = useQueryClient();
+  const apiClient = createApiClient("http://localhost:3000/v1");
+
   const { isPending, isFetching, isError, data, error } = useQuery({
     queryKey: ["ApiRoot"],
-    queryFn: async () => {
-      await new Promise((resolve) => setTimeout(resolve, 2000));
-
-      const response = await fetch("http://localhost:3000/v1/");
-      if (!response.ok) {
-        throw new Error("Network response was not ok");
-      }
-      return response.json();
-    },
+    queryFn: async () => apiClient.get(""),
   });
 
   const handleRefresh = () => {
@@ -42,7 +60,7 @@ function CheckApiStatus() {
         </>
       )}
       {isPending && <Spinner size="lg" />}
-      {!isPending && !isError && data && (
+      {!isPending && !isError && (
         <BlockQuote className="max-h-80">
           <pre>{JSON.stringify(data, null, 2)}</pre>
         </BlockQuote>
@@ -67,12 +85,60 @@ function CheckApiStatus() {
         </>
       )}
       <Separator className="my-4" />
+      <CodeBlock code={imp} />
+      <CodeBlock code={code} />
+      <CodeBlock code={usage} />
     </Tile>
   );
 }
 
-function DatabaseGetDataFromButton() {
-  return <div>get data from button</div>;
+interface ExampleDataType {
+  name: string;
+  count: number;
+  isActive: boolean;
+}
+
+function TableFromDatabase() {
+  const apiClient = createApiClient("http://localhost:3000/v1/example");
+
+  const { isPending, isFetching, isError, data, error } = useQuery<
+    ExampleDataType[]
+  >({
+    queryKey: ["ExampleSummaries"],
+    queryFn: async () => apiClient.get("summaries"),
+  });
+  return (
+    <Tile
+      title="Table from Database"
+      description="Call the API to get data from a database and put it in a table."
+    >
+      {isPending && <Spinner size="lg" />}
+      {isError && (
+        <div>An error has occurred: {error?.message || "Unknown error"}</div>
+      )}
+      {!isError && !isPending && !isFetching && (
+        <DataTableCore
+          columns={[
+            {
+              header: "Name",
+              accessorKey: "name",
+            },
+            {
+              header: "Count",
+              accessorKey: "count",
+            },
+            {
+              header: "Active",
+              accessorKey: "isActive",
+            },
+          ]}
+          data={data || []}
+          toolbar={(table) => <DataTableToolbar table={table} />}
+          caption="This data comes from database, via custom DTO, reusable apiClient, and react-query!"
+        />
+      )}
+    </Tile>
+  );
 }
 
 const sections = [
@@ -84,7 +150,7 @@ const sections = [
   {
     id: "database-data-from-button",
     title: "Database Data From Button",
-    children: <DatabaseGetDataFromButton />,
+    children: <TableFromDatabase />,
   },
 ];
 const DatabaseExample = () => {
